Logit CVO, loss_inp, masking, dtaloader 1. Resolve error in cvo with softmax logits 1.1. Fixed implementation of PtSampleInGridAngle 1.2. Fixed mode switching between use_panoptic and not 2. Fixed flipped sign of loss_inp 3. Fill holes in depth_mask 4. Fixed fataloader killed by signal (only when running two in paral)

cvo_utils.py
@@ -1,5 +1,5 @@
 import torch
-import cvo_dense_samp
+import cvo_dense_samp, cvo_dense_angle
 from torch.autograd import Function
 
 class PtSampleInGrid(Function):
@@ -24,13 +24,12 @@ class PtSampleInGrid(Function):
 
 class PtSampleInGridAngle(Function):
     @staticmethod
-    def forward(ctx, pts, pts_info, grid_source, grid_valid, neighbor_range, ell, ignore_ib=False):
+    def forward(ctx, pts, pts_info, grid_source, grid_valid, neighbor_range, ignore_ib=False):
         """ pts: B*2*N, pts_info: B*C*N, grid_source: B*C*H*W (C could be xyz, rgb, ...), grid_valid: B*1*H*W, neighbor_range: int
         """
-        outputs = cvo_dense_samp.forward(pts, pts_info, grid_source, grid_valid, neighbor_range, ell, ignore_ib)
+        outputs = cvo_dense_angle.forward(pts, pts_info, grid_source, grid_valid, neighbor_range, ignore_ib)
         ctx.save_for_backward(pts, pts_info, grid_source, grid_valid)
         ctx.neighbor_range = neighbor_range
-        ctx.ell = ell
         ctx.ignore_ib = ignore_ib
         return outputs
 
@@ -38,5 +37,5 @@ class PtSampleInGridAngle(Function):
     def backward(ctx, dy):
         pts, pts_info, grid_source, grid_valid = ctx.saved_tensors
         dy = dy.contiguous()
-        dx1, dx2 = cvo_dense_samp.backward(dy, pts, pts_info, grid_source, grid_valid, ctx.neighbor_range, ctx.ell, ctx.ignore_ib)
+        dx1, dx2 = cvo_dense_angle.backward(dy, pts, pts_info, grid_source, grid_valid, ctx.neighbor_range, ctx.ignore_ib)
         return None, dx1, dx2, None, None, None, None

datasets/mono_dataset.py
@@ -94,7 +94,7 @@ class MonoDataset(data.Dataset):
         # ### for dilation operation to generate mask for valid pixels
         self.dilate_struct = {}
         for i in range(self.num_scales):
-            scale_d = max(5 - 2*i, 1)
+            scale_d = max(35 - 8*i, 1) # 5
             self.dilate_struct[i] = np.ones((scale_d, scale_d))
 
     def preprocess(self, inputs, color_aug):
@@ -252,7 +252,9 @@ class MonoDataset(data.Dataset):
 
                     ### generate mask from the lidar points
                     # mask = binary_dilation(depth_gt, self.dilate_struct[j])
-                    mask = binary_closing(depth_gt, self.dilate_struct[j])
+                    mask = depth_gt.copy()
+                    mask[int(new_h/2):] = 1
+                    mask = binary_closing(mask, self.dilate_struct[j])
                     mask = np.expand_dims(mask, 0)
                     inputs[("depth_mask", i, j)] = torch.from_numpy(mask)
 

trainer.py
@@ -71,7 +71,7 @@ class Trainer:
         self.models = {}
         self.parameters_to_train = []
 
-        self.device = torch.device("cpu" if self.opt.no_cuda else "cuda:0")
+        self.device = torch.device("cpu" if self.opt.no_cuda else "cuda:1")
 
         self.num_scales = len(self.opt.scales)
         self.num_input_frames = len(self.opt.frame_ids)
@@ -698,8 +698,9 @@ class Trainer:
                     outputs[("flat_xyz", frame_id, scale, wrap_id, gt_flag)] = {}
                     outputs[("flat_uv", frame_id, scale, wrap_id, gt_flag)] = {}
                     outputs[("flat_hsv", frame_id, scale, wrap_id, gt_flag)] = {}
-                    outputs[("flat_panop", frame_id, scale, wrap_id, gt_flag)] = {}
-                    outputs[("flat_seman", frame_id, scale, wrap_id, gt_flag)] = {}
+                    if self.opt.use_panoptic:
+                        outputs[("flat_panop", frame_id, scale, wrap_id, gt_flag)] = {}
+                        outputs[("flat_seman", frame_id, scale, wrap_id, gt_flag)] = {}
                     for ib in range(self.opt.batch_size):
                         Ti = T[ib:ib+1]
                         Ki = inputs[("K", scale)][ib:ib+1, :3, :3]
@@ -791,18 +792,22 @@ class Trainer:
         self.dist_combos = [(0, 1, True, False), (0, 0, True, False), (0, -1, True, False)]
         # inp_combos = self.inp_combo_from_dist_combo(dist_combos)
 
-        self.feats_cross = ["xyz", "seman"]
-        self.feats_self = ["xyz", "panop"]
+        if self.opt.use_panoptic:
+            self.feats_cross = ["xyz", "seman"]
+            self.feats_self = ["xyz", "panop"]
+        else:
+            self.feats_cross = ["xyz", "hsv"]
+            self.feats_self = ["xyz", "hsv"]
         inp_feat_combos = self.inp_feat_combo_from_dist_combo(self.dist_combos)
 
         # feats_needed = ["xyz", "hsv"]
         feats_ell = {}
         feats_ell["xyz"] = 0.1
         feats_ell["hsv"] = 0.2
-        feats_ell["panop"] = 0.2
-        feats_ell["seman"] = 0.2
+        feats_ell["panop"] = 0.2    # in Angle mode this is not needed
+        feats_ell["seman"] = 0.2    # in Angle mode this is not needed
         
-        neighbor_range = int(3)
+        neighbor_range = int(2)
         inp_feat_dict = {}
         for combo in inp_feat_combos:
             inp_feat_dict[combo] = {}
@@ -822,7 +827,7 @@ class Trainer:
                         # print("flat_uv.shape", flat_uv.shape)
                         # print("flat_info.shape", flat_info.shape)
                         # print("grid_info.shape", grid_info.shape)
-                        inn_single = PtSampleInGrid.apply(flat_uv.contiguous(), flat_info.contiguous(), grid_info.contiguous(), grid_valid.contiguous(), neighbor_range, ell, True)
+                        inn_single = PtSampleInGridAngle.apply(flat_uv.contiguous(), flat_info.contiguous(), grid_info.contiguous(), grid_valid.contiguous(), neighbor_range, True) # PtSampleInGrid
                         inn_list.append(inn_single)
                         # print("inn_single.shape", inn_single.shape)
                     inp_feat_dict[combo][scale] = torch.cat(inn_list, dim=2) #1 * NN * N
@@ -831,9 +836,11 @@ class Trainer:
                     flat_uv = outputs[("flat_uvb", flat_idx, scale, grid_idx, flat_gt)]
                     flat_info = outputs[("flat_"+feat, flat_idx, scale, grid_idx, flat_gt)]
                     grid_info = outputs[("grid_"+feat, grid_idx, scale, grid_idx, grid_gt)]
-                    inp_feat_dict[combo][scale] = PtSampleInGrid.apply(flat_uv.contiguous(), flat_info.contiguous(), grid_info.contiguous(), grid_valid.contiguous(), neighbor_range, ell)
+                    if feat == "seman":
+                        inp_feat_dict[combo][scale] = PtSampleInGridAngle.apply(flat_uv.contiguous(), flat_info.contiguous(), grid_info.contiguous(), grid_valid.contiguous(), neighbor_range)
+                    else:
+                        inp_feat_dict[combo][scale] = PtSampleInGrid.apply(flat_uv.contiguous(), flat_info.contiguous(), grid_info.contiguous(), grid_valid.contiguous(), neighbor_range, ell)
                     # print("inp_feat_dict[{}][{}].shape".format(combo, scale), inp_feat_dict[combo][scale].shape)
-
         
         inp_dict, dist_dict, cos_dict = self.get_dist_from_inp_grid_flat(self.dist_combos, inp_feat_dict)
 
@@ -871,7 +878,7 @@ class Trainer:
             cos_dict[combo] = {}
             inp_dict[combo] = {}
             for scale in self.opt.scales:
-                inp_dict[combo][scale] = innerp_dict[tags[0]][scale]
+                inp_dict[combo][scale] = - innerp_dict[tags[0]][scale] # fixed Jan 6!
                 dist_dict[combo][scale] = innerp_dict[tags[1]][scale] + innerp_dict[tags[2]][scale] - 2 * innerp_dict[tags[0]][scale]
                 cos_dict[combo][scale] = 1 - innerp_dict[tags[0]][scale] / torch.sqrt( innerp_dict[tags[1]][scale] * innerp_dict[tags[2]][scale] )
 