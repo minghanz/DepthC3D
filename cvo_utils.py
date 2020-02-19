Change depth-disp conversion, grad, opt from file 1. Disp changed from invrese depth to r/(D+r) with min and max eps. 2. Depth gradient padding changed to fix the problem of artificial at bottom. 3. Load options from yaml file to be more coordinated and avoid missing options.

cvo_utils.py
@@ -223,7 +223,8 @@ class SobelGrad(torch.nn.Module):
         kern_y = kern_x.transpose(2,3)
         self.register_buffer("kern_x", kern_x)
         self.register_buffer("kern_y", kern_y)
-        self.pad_layer = torch.nn.ReflectionPad2d(1) # (left,right,top,bottom) or an int
+        # self.pad_layer = torch.nn.ReflectionPad2d(1) # (left,right,top,bottom) or an int
+        self.pad_layer = torch.nn.ReplicationPad2d(1)
         ## use a dedicated padding layer because the padding in F.conv2d only pads zeros.
 
     def forward(self, img):
@@ -231,6 +232,7 @@ class SobelGrad(torch.nn.Module):
         Otherwise the return channel number is the same as input
         """
         img_pad = self.pad_layer(img)
+        img_pad[:,:,-1, :] = 2 * img_pad[:,:,-2, :] - img_pad[:,:,-3, :]        ## so that the last row's vertical gradient is decided by the last two rows
         grad_x = torch.zeros_like(img)
         grad_y = torch.zeros_like(img)
         for ic in range(img.shape[1]):

err_train.py
@@ -22,7 +22,7 @@ def error_disp(disp, gt_depth, opt):
     # disp = F.interpolate(
     #             disp, [gt_height, gt_width], mode="bilinear", align_corners=False)
 
-    scaled_disp, pred_depth = disp_to_depth(disp, opt.min_depth, opt.max_depth)
+    scaled_disp, pred_depth = disp_to_depth(disp, opt.min_depth, opt.max_depth, opt.ref_depth, opt.depth_ref_mode)
 
     ###################### switch to eval mode ##########################
     # pred_depth_np = pred_depth.cpu().numpy()[0,0]

layers.py
@@ -13,25 +13,39 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def disp_to_depth(disp, min_depth, max_depth):
+def disp_to_depth(disp, min_depth, max_depth, ref_depth=0, depth_ref_mode=False):
     """Convert network's sigmoid output into depth prediction
     The formula for this conversion is given in the 'additional considerations'
     section of the paper.
     """
-    min_disp = 1 / max_depth
-    max_disp = 1 / min_depth
-    scaled_disp = min_disp + (max_disp - min_disp) * disp
-    depth = 1 / scaled_disp
+    if not depth_ref_mode:
+        min_disp = 1 / max_depth
+        max_disp = 1 / min_depth
+        scaled_disp = min_disp + (max_disp - min_disp) * disp
+        depth = 1 / scaled_disp
+    else:
+        eps_a = ref_depth / (max_depth + ref_depth)
+        eps_b = min_depth
+        depth = ref_depth / (disp + eps_a) - ref_depth + eps_b
+        scaled_disp = disp
+
     return scaled_disp, depth
 
 ## ZMH: added by me. to visualize gt depth as disp
-def depth_to_disp(depth, min_depth, max_depth):
-    min_disp = 1 / max_depth
-    max_disp = 1 / min_depth
-    # scaled_disp = torch.where(depth > 0, 1 / depth, 0)
-    zeros_ = torch.zeros_like(depth)
-    scaled_disp = torch.where(depth > 0, torch.clamp(1 / depth, min=min_disp, max=max_disp), zeros_)
-    disp = torch.where(scaled_disp > 0, ( scaled_disp - min_disp ) / (max_disp - min_disp), zeros_)
+def depth_to_disp(depth, min_depth, max_depth, ref_depth=0, depth_ref_mode=False):
+    if not depth_ref_mode:
+        min_disp = 1 / max_depth
+        max_disp = 1 / min_depth
+        # scaled_disp = torch.where(depth > 0, 1 / depth, 0)
+        zeros_ = torch.zeros_like(depth)
+        scaled_disp = torch.where(depth > 0, torch.clamp(1 / depth, min=min_disp, max=max_disp), zeros_)
+        disp = torch.where(scaled_disp > 0, ( scaled_disp - min_disp ) / (max_disp - min_disp), zeros_)
+    else:
+        eps_a = ref_depth / (max_depth + ref_depth)
+        eps_b = min_depth
+        zeros_ = torch.zeros_like(depth)
+        clamped_depth = torch.where(depth > 0, torch.clamp(depth, min=min_depth, max=max_depth), zeros_ )
+        disp = ref_depth / (clamped_depth + ref_depth - eps_b) - eps_a
     return disp
 
 def transformation_from_parameters(axisangle, translation, invert=False):

trainer.py
@@ -70,7 +70,16 @@ def my_collate_fn(batch):
 class Trainer:
     def __init__(self, options, ups_arg, ups_cfg):
         self.opt = options
-        self.log_path = os.path.join(self.opt.log_dir, self.opt.model_name)
+        if self.opt.server == "mcity":
+            self.log_dir = "/mnt/storage8t/minghanz/tmp"
+        elif self.opt.server == "sunny":
+            self.log_dir = "/media/sda1/minghanz/tmp"
+        elif self.opt.server == "home":
+            self.log_dir = os.path.join(script_path, "tmp")
+        else:
+            raise ValueError("server {} not recognized.".format(self.opt.server))
+
+        self.log_path = os.path.join(self.log_dir, self.opt.model_name)
         torch_vs = (torch.__version__).split('.')
         self.torch_version = float(torch_vs[0]) + 0.1 * float(torch_vs[1])
 
@@ -168,7 +177,7 @@ class Trainer:
             self.load_model()
 
         print("Training model named:\n  ", self.opt.model_name)
-        print("Models and tensorboard events files are saved to:\n  ", self.opt.log_dir)
+        print("Models and tensorboard events files are saved to:\n  ", self.log_dir)
         print("Training is using:\n  ", self.device)
 
         # data
@@ -317,20 +326,26 @@ class Trainer:
         if self.opt.disable_log:
             self.path_model = None
             self.path_opt = None
-            self.path_pcd = None
             self.writers = None
-            self.nkern_path = None
         else:
             self.path_model = os.path.join(self.log_path, "models" + "_"+self.ctime)
             self.path_opt = self.path_model
-            self.path_pcd = os.path.join(self.log_path, "pcds_"+self.ctime)
             self.writers = {}
             for mode in ["train", "val", "val_set"]:
-                # self.writers[mode] = SummaryWriter(os.path.join(self.log_path, mode))
                 self.writers[mode] = SummaryWriter(os.path.join(self.log_path, mode + '_' + self.ctime))
+        
+        if self.opt.save_pic_intv != 0:
             self.nkern_path = os.path.join(self.log_path, "nkerns_"+self.ctime)
             if not os.path.exists(self.nkern_path):
                 os.makedirs(self.nkern_path)
+        else:
+            self.nkern_path = None
+        if self.opt.save_pcd_intv != 0:
+            self.path_pcd = os.path.join(self.log_path, "pcds_"+self.ctime)
+            if not os.path.exists(self.path_pcd):
+                os.makedirs(self.path_pcd)
+        else:
+            self.path_pcd = None
 
         if not self.opt.no_ssim:
             self.ssim = SSIM()
@@ -799,7 +814,7 @@ class Trainer:
                 disp, [self.height_dict[self.cur_datasrc], self.width_dict[self.cur_datasrc]], mode="bilinear", align_corners=False)
             source_scale = 0
 
-        _, depth = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
+        _, depth = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth, self.opt.ref_depth, self.opt.depth_ref_mode)
         return depth, source_scale
 
     def gen_pcl_gt(self, inputs, outputs, disp, scale, frame_id, T_inv=None):
@@ -2225,7 +2240,7 @@ class Trainer:
                 else:
                     disp = outputs_others[frame_id][("disp", scale)]
                 depth_gt = inputs[("depth_gt_scale", frame_id, scale)]
-                disp_gt = depth_to_disp(depth_gt, self.opt.min_depth, self.opt.max_depth)
+                disp_gt = depth_to_disp(depth_gt, self.opt.min_depth, self.opt.max_depth, self.opt.ref_depth, self.opt.depth_ref_mode)
                 # print("disp_gt range", torch.min(disp_gt), torch.max(disp_gt))
                 if frame_id == self.opt.frame_ids[0]:
                     disp_losses[scale] = torch.tensor(0, dtype=torch.float32, device=self.device)
@@ -2249,7 +2264,7 @@ class Trainer:
                 else:
                     disp = outputs_others[frame_id][("disp", scale)]
                 depth_gt = inputs[("depth_gt_scale", frame_id, scale)]
-                _, depth_pred = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
+                _, depth_pred = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth, self.opt.ref_depth, self.opt.depth_ref_mode)
                 # print("disp_gt range", torch.min(disp_gt), torch.max(disp_gt))
                 if frame_id == self.opt.frame_ids[0]:
                     depth_losses[scale] = torch.tensor(0, dtype=torch.float32, device=self.device)
@@ -2283,7 +2298,7 @@ class Trainer:
                     self.get_grid_flat_normal(outputs, n_pts_dict)
                 
 
-                if self.step % 4000 == 0: ## ZMH: comment this line for memory leak debug! 
+                if self.step % self.opt.save_pcd_intv == 0: ## ZMH: comment this line for memory leak debug! 
                     self.save_pcd(outputs, n_pts_dict)
 
                 innerps, dists, coss = self.get_innerp_from_grid_flat(inputs, outputs, n_pts_dict)
@@ -2730,7 +2745,7 @@ class Trainer:
 
                 # if s == 0:
                     # print('shape 1', outputs[("disp", s)][j].shape)
-                disp = depth_to_disp(inputs[("depth_gt_scale", 0, s)][j], self.opt.min_depth, self.opt.max_depth)
+                disp = depth_to_disp(inputs[("depth_gt_scale", 0, s)][j], self.opt.min_depth, self.opt.max_depth, self.opt.ref_depth, self.opt.depth_ref_mode)
                 # disp = disp.squeeze(1)
                 # print('shape 2', disp.shape)
                 writer.add_image(